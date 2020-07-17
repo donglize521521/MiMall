f431a3db959ee5ef20ed8f64da4b61121c218c3f
donglize521521 <1486252015@qq.com>
2020-07-17 08:22:09 +0800

    目录设置

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import jsonp from 'jsonp'
+
 
 export default {
   name: 'App',
@@ -22,15 +22,9 @@ export default {
   mounted(){
  
 }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
